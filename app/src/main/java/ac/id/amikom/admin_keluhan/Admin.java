c78e42f08ae3bbb5c50374f85cec8ff6f9544184
Lathifah1803 <lthfhhhhh2@gmail.com>
2020-09-24 11:06:14 +0700

    register

---
+++ app/src/main/java/ac/id/amikom/admin_keluhan/Admin.java
@@ -0,0 +1,18 @@
+package ac.id.amikom.admin_keluhan;
+
+
+
+public class Admin {
+
+    public String username;
+    public String email;
+
+    public Admin() {
+        // Default constructor required for calls to DataSnapshot.getValue(User.class)
+    }
+
+    public Admin(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+}
--- app/src/main/java/ac/id/amikom/admin_keluhan/RegisterActivity.java
+++ app/src/main/java/ac/id/amikom/admin_keluhan/RegisterActivity.java
@@ -1,56 +1,187 @@
 package ac.id.amikom.admin_keluhan;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.InputType;
+import android.text.TextUtils;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-public class RegisterActivity extends AppCompatActivity {
 
-    private EditText edtEmail, edtPassword;
-    private Button btnRegister;
-    private FirebaseAuth auth;
+public class RegisterActivity extends AppCompatActivity  implements View.OnClickListener {
+
+    //deklarasi beberapa variabel seperti button editext, databaserefrence, firebaseauth
+    private static final String TAG = "LoginActivity";
+
+    private DatabaseReference mDatabase;
+    private FirebaseAuth mAuth;
+    private EditText edtEmail;
+    private EditText edtPass;
+    private Button btnMasuk;
+    private Button btnDaftar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        initView();
+        //variabel tadi untuk memanggil fungsi
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
+
+        // diatur sesuai id komponennya
+        edtEmail = (EditText) findViewById(R.id.tv_email);
+        edtPass = (EditText) findViewById(R.id.tv_pass);
+        btnMasuk = (Button) findViewById(R.id.btn_masuk);
+        btnDaftar = (Button) findViewById(R.id.btn_daftar);
+
+        //nambahin method onClick, biar tombolnya bisa diklik
+        btnMasuk.setOnClickListener(this);
+        btnDaftar.setOnClickListener(this);
     }
 
-    private  void initView() {
-        edtEmail = findViewById(R.id.edt_email_register);
-        edtPassword = findViewById(R.id.edt_password_register);
-        btnRegister = findViewById(R.id.btn_sign_up);
-        auth = FirebaseAuth.getInstance();
+    //fungsi signin untuk mengkonfirmasi data pengguna yang sudah mendaftar sebelumnya
+    private void signIn() {
+        Log.d(TAG, "signIn");
+        if (!validateForm()) {
+            return;
+        }
+
+        //showProgressDialog();
+        String email = edtEmail.getText().toString();
+        String password = edtPass.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signIn:onComplete:" + task.isSuccessful());
+                        //hideProgressDialog();
+
+                        if (task.isSuccessful()) {
+                            onAuthSuccess(task.getResult().getUser());
+                        } else {
+                            Toast.makeText(RegisterActivity.this, "Sign In Failed",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 
-    private void registerUser() {
-        btnRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //menampung imputan user
-                String emailUser = edtEmail.getText().toString().trim();
-                String passwordUser = edtPassword.getText().toString().trim();
+    //fungsi ini untuk mendaftarkan data pengguna ke Firebase
+    private void signUp() {
+        Log.d(TAG, "signUp");
+        if (!validateForm()) {
+            return;
+        }
 
+        //showProgressDialog();
+        String email = edtEmail.getText().toString();
+        String password = edtPass.getText().toString();
 
-                //validasi email dan password
-                //jika email ksoong
-                if (emailUser.isEmpty()) {
-                    edtEmail.setError("Email tidak boleh kosong");
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "createUser:onComplete:" + task.isSuccessful());
+                        //hideProgressDialog();
 
-                }
-                //jika email tidak valid
-                else if (!Patterns.EMAIL_ADDRESS.matcher(emailUser).matches()) {
-                    edtEmail.setError("Email tidak valid");
-                }
-            }
-        });
+                        if (task.isSuccessful()) {
+                            onAuthSuccess(task.getResult().getUser());
+                        } else {
+                            Toast.makeText(RegisterActivity.this, "Sign Up Failed",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
+
+    //fungsi dipanggil ketika proses Authentikasi berhasil
+    private void onAuthSuccess(FirebaseUser user) {
+        String username = usernameFromEmail(user.getEmail());
+
+        // membuat User admin baru
+        writeNewAdmin(user.getUid(), username, user.getEmail());
+
+        // Go to MainActivity
+        startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+        finish();
+    }
+
+    /*
+        ini fungsi buat bikin username dari email
+            contoh email: abcdefg@mail.com
+            maka username nya: abcdefg
+     */
+    private String usernameFromEmail(String email) {
+        if (email.contains("@")) {
+            return email.split("@")[0];
+        } else {
+            return email;
+        }
+    }
+
+    //fungsi untuk memvalidasi EditText email dan password agar tak kosong dan sesuai format
+    private boolean validateForm() {
+        boolean result = true;
+        if (TextUtils.isEmpty(edtEmail.getText().toString())) {
+            edtEmail.setError("Required");
+            result = false;
+        } else {
+            edtEmail.setError(null);
+        }
+
+        if (TextUtils.isEmpty(edtPass.getText().toString())) {
+            edtPass.setError("Required");
+            result = false;
+        } else {
+            edtPass.setError(null);
+        }
+
+        return result;
+    }
+
+    // menulis ke Database
+    private void writeNewAdmin(String userId, String name, String email) {
+        Admin admin = new Admin(name, email);
+
+        mDatabase.child("admins").child(userId).setValue(admin);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.btn_masuk) {
+            signIn();
+        } else if (i == R.id.btn_daftar) {
+            signUp();
+        }
+    }
+
 }
